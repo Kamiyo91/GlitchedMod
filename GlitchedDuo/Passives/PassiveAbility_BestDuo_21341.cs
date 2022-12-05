e20f839f1127d514d807b2105eb14dafda642fa2
Kamiyo <kamiyo.k91@gmail.com>
2022-12-05 19:21:57 +0100

    Hotfix

--- GlitchedDuo/Passives/PassiveAbility_BestDuo_21341.cs
+++ GlitchedDuo/Passives/PassiveAbility_BestDuo_21341.cs
@@ -6,71 +6,29 @@ namespace GlitchedMod.GlitchedDuo.Passives
     public class PassiveAbility_BestDuo_21341 : PassiveAbilityBase
     {
         private bool _buffActive;
-        private int _memberCount;
 
         public override void OnWaveStart()
         {
-            _memberCount = UnitUtil.SupportCharCheck(owner);
-            if (_memberCount == 1 && BattleObjectManager.instance.GetAliveList(owner.faction)
+            if (BattleObjectManager.instance.GetAliveList(owner.faction)
                     .Count(x => x.passiveDetail.PassiveList.Any(y => y is PassiveAbility_BestDuo_21341)) == 2)
             {
-                owner.bufListDetail.AddKeywordBufThisRoundByEtc(KeywordBuf.Strength, 2);
-                owner.bufListDetail.AddKeywordBufThisRoundByEtc(KeywordBuf.Endurance, 2);
+                owner.bufListDetail.AddKeywordBufByEtc(KeywordBuf.Strength, 2);
+                owner.bufListDetail.AddKeywordBufByEtc(KeywordBuf.Endurance, 2);
                 _buffActive = true;
-                return;
             }
-
-            switch (_memberCount)
-            {
-                case 1:
-                    if (BattleObjectManager.instance.GetAliveList(owner.faction)
-                            .Count(x => x.passiveDetail.PassiveList.Any(y => y is PassiveAbility_BestDuo_21341)) == 2)
-                    {
-                        owner.bufListDetail.AddKeywordBufByEtc(KeywordBuf.Strength, 2);
-                        owner.bufListDetail.AddKeywordBufByEtc(KeywordBuf.Endurance, 2);
-                        _buffActive = true;
-                        break;
-                    }
-
-                    _buffActive = false;
-                    break;
-                case 2:
-                    owner.bufListDetail.AddKeywordBufThisRoundByEtc(KeywordBuf.Strength, 2);
-                    owner.bufListDetail.AddKeywordBufThisRoundByEtc(KeywordBuf.Endurance, 2);
-                    _buffActive = true;
-                    break;
-                default:
-                    _buffActive = false;
-                    break;
-            }
+            else _buffActive = false;
         }
 
         public override void OnRoundEnd()
         {
-            _memberCount = UnitUtil.SupportCharCheck(owner);
-            switch (_memberCount)
+            if (BattleObjectManager.instance.GetAliveList(owner.faction)
+                    .Count(x => x.passiveDetail.PassiveList.Any(y => y is PassiveAbility_BestDuo_21341)) == 2)
             {
-                case 1:
-                    if (BattleObjectManager.instance.GetAliveList(owner.faction)
-                            .Count(x => x.passiveDetail.PassiveList.Any(y => y is PassiveAbility_BestDuo_21341)) == 2)
-                    {
-                        owner.bufListDetail.AddKeywordBufByEtc(KeywordBuf.Strength, 2);
-                        owner.bufListDetail.AddKeywordBufByEtc(KeywordBuf.Endurance, 2);
-                        _buffActive = true;
-                        break;
-                    }
-
-                    _buffActive = false;
-                    break;
-                case 2:
-                    owner.bufListDetail.AddKeywordBufByEtc(KeywordBuf.Strength, 2);
-                    owner.bufListDetail.AddKeywordBufByEtc(KeywordBuf.Endurance, 2);
-                    _buffActive = true;
-                    break;
-                default:
-                    _buffActive = false;
-                    break;
+                owner.bufListDetail.AddKeywordBufByEtc(KeywordBuf.Strength, 2);
+                owner.bufListDetail.AddKeywordBufByEtc(KeywordBuf.Endurance, 2);
+                _buffActive = true;
             }
+            else _buffActive = false;
         }
 
         public bool GetBuffStatus()
